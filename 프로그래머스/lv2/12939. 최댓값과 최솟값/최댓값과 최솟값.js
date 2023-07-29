a5942cc743d50bb1bb83ebae90cf4dab37b9e796
HongJungKim(이든) <58525009+HongJungKim-dev@users.noreply.github.com>
2023-07-30 00:03:40 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,9 +1,8 @@
 function solution(s) {
     const numbers = s.split(' ')
-                        .map((value) => Number(value))
-                            .sort((aNumber, bNumber) => aNumber - bNumber);
+                        .map((value) => Number(value));
     const min = numbers[0];
     const max = numbers[numbers.length - 1];
 
-    return `${min} ${max}`;
+    return `${Math.min(...numbers)} ${Math.max(...numbers)}`;
 }
