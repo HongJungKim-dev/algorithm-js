f0b4986efcb69df647bb4ee9f07f64fa8867dd1c
HongJungKim(이든) <58525009+HongJungKim-dev@users.noreply.github.com>
2023-08-11 23:42:07 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -1,9 +1,5 @@
 function solution(a, b) {
-    const numbers = [a, b];
-    numbers.sort((a, b) => a - b);
-    const min = numbers[0];
-    const max = numbers[1];
-    const num = (max - min + 1);
+    const num = (Math.abs(a - b) + 1);
     
-    return num * (min + max) / 2;
+    return num * (a + b) / 2;
 }
