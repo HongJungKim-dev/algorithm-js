747c3dc56187c7946f330c049f601325ad945864
HongJungKim(이든) <58525009+HongJungKim-dev@users.noreply.github.com>
2023-10-02 00:01:12 +0900

    [unrated] Title: 원소들의 곱과 합, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.js
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -1,9 +1,5 @@
 function solution(num_list) {
-
-    let multi = 1;
-    for (const element of num_list) {
-        multi *= element;
-    }
+    const multi = num_list.reduce((acc, cur) => acc * cur, 1);
     const sum = num_list.reduce((acc, cur) => acc + cur, 0);
 
     return multi < sum ** 2 ? 1 : 0;
