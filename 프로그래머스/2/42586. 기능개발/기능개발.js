51c9285a873dbbfb68a55b46e653614db1df22e5
HongJungKim <58525009+HongJungKim-dev@users.noreply.github.com>
2025-04-23 21:23:21 +0900

    [level 2] Title: 기능개발, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,39 +1,29 @@
 function solution(progresses, speeds) {
-    const answer = [];
-    const days = [];
-    
-    for (let i = 0; i < progresses.length; i++) {
-        const progress = progresses[i];
-        const speed = speeds[i];
-        const result = Math.ceil((100 - progress) / speed);
-        days.push(result)
-    }
-
     let stack = [];
-    let max = 0;
-    for (const element of days) {
-        const target = stack.pop();
-        if (target === undefined) {
-            max = element;
-            stack.push(element);
+    const answer = [];
+    const progressesLength = progresses.length;
+    let count = 0;
+    for (let i = 0; i < progressesLength; i++) {
+        const progress = progresses[i];
+        const remain = 100 - progress;
+        const speed = speeds[i];
+        const cost = Math.ceil(remain / speed);
+        
+        if (stack.length === 0) {
+            stack.push(cost);
             continue;
         }
         
-        if (max < element) {
-            answer.push(stack.length + 1);
-            stack = [];
-            stack.push(element);
-            if(max < element) {
-                max = element;
-            }
+        const max = Math.max(...stack);
+        
+        if (max < cost) {
+            answer.push(stack.length);
+            stack = [cost];
         } else {
-            stack.push(target);
-            stack.push(element);
+            stack.push(cost);
         }
-        
-        
     }
-
+    
     if (stack.length > 0) {
         answer.push(stack.length);
     }
