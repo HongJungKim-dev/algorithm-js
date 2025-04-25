8700c45332813ba0ea2f36af78a523fd2589711d
HongJungKim <58525009+HongJungKim-dev@users.noreply.github.com>
2025-04-26 00:37:17 +0900

    [level 2] Title: 올바른 괄호, Time: 32.75 ms, Memory: 39.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,19 +1,21 @@
 function solution(s){
     const arr = s.split('')
     const stack = [];
-    for(const element of arr) {
-        const target = stack.pop();
-        if (target === undefined) {
-            stack.push(element)
-            continue;
+    
+    for (const element of arr) {
+        if (element === '(') {
+            stack.push(element);
+        } else if (element === ')') {
+            if (stack.length === 0) {
+                stack.push(element);
+                continue;
+            }
+            const popped = stack.pop();
+            if (popped !== '(') {
+                stack.push(popped);
+            }
         }
-        if (element === ')' && target === '(') {
-            continue;
-        }
-        
-        stack.push(target);
-        stack.push(element);
     }
 
-    return stack.length ===0;
+    return stack.length === 0;
 }
