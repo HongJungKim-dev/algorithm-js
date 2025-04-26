2ce63ff79cc84fcfe703a909e06017d321a7c718
HongJungKim <58525009+HongJungKim-dev@users.noreply.github.com>
2025-04-26 11:20:18 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,8 +1,10 @@
 function solution(array, commands) {
     const answer = commands.map((command) => {
         const [start, end, targetIndex] = command;
-        const slicedArr = array.slice(start - 1, end).sort((a, b) => a - b);
-        return slicedArr[targetIndex - 1]
+        const newArray = array.slice(start - 1, end);
+        newArray.sort((a, b) => a - b);
+        
+        return newArray[targetIndex - 1];
     })
     return answer;
 }
