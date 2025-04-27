19e60c561d821a0c02d8709ef364ec3a1a5db414
HongJungKim <58525009+HongJungKim-dev@users.noreply.github.com>
2025-04-27 20:02:25 +0900

    [level 2] Title: 타겟 넘버, Time: 15.78 ms, Memory: 35.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,22 +1,18 @@
 function solution(numbers, target) {
-    var answer = 0;
-    let count = 0;
-    
-    function dfs(index, sum) {
-        
-        if (index === numbers.length) {
+    const visited = Array(numbers.length).fill(false);
+    let answer = 0;
+    const dfs = (index, sum) => {
+        if(index === numbers.length) {
             if (sum === target) {
-                count +=1 ;
+                answer += 1;
             }
             return;
         }
-        
-        dfs(index + 1, sum - numbers[index]);
-        
         dfs(index + 1, sum + numbers[index]);
+        dfs(index + 1, sum - numbers[index]);
     }
-    
-    dfs(0, 0)
-    
-    return count;
+    dfs(0, 0);
+
+    return answer;
 }
+
