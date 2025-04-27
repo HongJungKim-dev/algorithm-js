c8705e9e32ca3b4f3d46aeea5fd951ec7a82fded
HongJungKim <58525009+HongJungKim-dev@users.noreply.github.com>
2025-04-27 21:02:47 +0900

    [level 3] Title: 네트워크, Time: 0.45 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,42 +1,29 @@
 
 
 function solution(n, computers) {
-    const visited = Array.from(({ length: n }), () => false);
-
-    let count = 0;
-    
-    const bfs = (startNode) => {
-        const queue = [startNode];
-        visited[startNode] = true;
-
-        while(queue.length > 0) {
-            const currentNode = queue.shift();
-            for (let i = 0; i < n; i++) {
-                if(!visited[i] && computers[currentNode][i] === 1) {
-                    queue.push(i);
-               }
-            }
-        }
-
-    }
-    
-    const dfs = (node) => {
-        visited[node] = true;
-        
-        for (let i = 0; i < n; i++) {
-            if (!visited[i] && computers[node][i] === 1) {
-                dfs(i);
-                visited[i] = true;
-            }
+    let answer = 0;
+    const visited = Array.from({ length: n }, () => false);
+    const dfs = (index) => {
+        if(index === n) {
+            return;
         }
         
-    }
+        visited[index] = true;
         
+        for(let i = 0; i < n; i++) {
+            if(computers[index][i] === 1 && !visited[i]) {
+                  dfs(i);
+             }   
+        }
+    };
+    
     for(let i = 0; i < n; i++) {
         if(!visited[i]) {
             dfs(i);
-            count += 1;
+            answer += 1;
         }
+        
     }
-    return count;
+    
+    return answer;
 }
