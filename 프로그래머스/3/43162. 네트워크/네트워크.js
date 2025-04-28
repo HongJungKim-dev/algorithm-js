24dbc35ff6d67bd537597873bb970055251f338a
HongJungKim <58525009+HongJungKim-dev@users.noreply.github.com>
2025-04-28 23:37:43 +0900

    [level 3] Title: 네트워크, Time: 0.48 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -3,27 +3,27 @@
 function solution(n, computers) {
     let answer = 0;
     const visited = Array.from({ length: n }, () => false);
+    
     const dfs = (index) => {
-        if(index === n) {
+        if(n === index) {
             return;
-        }
+        };
         
         visited[index] = true;
         
         for(let i = 0; i < n; i++) {
             if(computers[index][i] === 1 && !visited[i]) {
-                  dfs(i);
-             }   
+                dfs(i);
+            }
         }
     };
     
-    for(let i = 0; i < n; i++) {
+    for(let i = 0; i < computers.length; i++) {
         if(!visited[i]) {
             dfs(i);
             answer += 1;
         }
-        
     }
-    
+
     return answer;
 }
